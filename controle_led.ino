b13b898b227c711316ae2b1f1f83df58a2550ea4
Fernando Vinicios Manchini de Souza <fernandomanchini@gmail.com>
2021-04-18 16:23:28 -0300

    Update controle_led.ino
    Alterado para ligar e desligar os led e cooler

--- controle_led.ino
+++ controle_led.ino
@@ -1,13 +1,19 @@
-int leds = 5;
-int modo = 1;
-int power = 255;
-int up = 1;
-int a = 0;
+int pinleds = 5;
+int pinCooler = 6;
+int modoOperacao = 1;
+int potenciaLed = 255;
+int potenciaCooler = 255;
+
+int botao = 0;
 
 void setup() {
-  pinMode(leds, OUTPUT);
+  pinMode(pinleds, OUTPUT);
+  pinMode(pinCooler, OUTPUT);
+  
   pinMode(4, OUTPUT);
+  pinMode(6, OUTPUT);
   digitalWrite(4, LOW);
+  digitalWrite(6, HIGH);
 
   Serial.begin(9600);
 }
@@ -15,77 +21,44 @@ void setup() {
 void loop() {
 
   if (Serial.available() > 0) {
-    modo = Serial.parseInt();
-    power = 0;
-    up = 1;
+    modoOperacao = Serial.parseInt();
+    potenciaLed = 0;
+   
   }
-   a = analogRead(7);
-   Serial.println(a);
-  if(a==1023){
-    a = analogRead(0);
+   botao = analogRead(0);
+   Serial.println(botao);
+   
+  if(botao==1023){
+    botao = analogRead(0);
+  }
+   if (botao == 740 || botao == 0) {
+    potenciaLed = 0;
+    modoOperacao++;
+    if (modoOperacao > 4) {
+      modoOperacao = 1;
+    }
   }
   
-  if (a == 740 || a == 0) {
-    power = 0;
-    up = 1;
-    modo++;
-    if (modo > 8) {
-      modo = 1;
-    }
-  }
 
-  if (modo == 1) { // sempre ligado
-    power = 255;
-  } else if (modo == 2) { // desligado
-    power = 0   ;
-  } else if (modo == 3) { // moda loca
-      power = random(255);
-  } else if (modo == 4) { // delay maciota
-    if (up == 1) {
-      power = power + 255/126;
-    } else {
-      power = power - 255/126;
-    }
-  }
-  else if (modo == 5) { // delay maciota
-    if (up == 1) {
-      power = power +  255/85;
-    } else {
-      power = power - 255/85;
-    }
-  } else if (modo == 6) { // delay maciota
-    if (up == 1) {
-      power = power +  255/62;
-    } else {
-      power = power - 255/62;
-    }
-  } else if (modo == 7) { // delay maciota
-    if (up == 1) {
-      power = power +  255/51;
-    } else {
-      power = power - 255/51;
-    }
-  }else if (modo == 8) { // delay maciota
-    if (up == 1) {
-      power = power +  100;
-    } else {
-      power = power - 100;
-    }
-  }
 
-  if (power > 254) {
-    up = 0;
-    power= 255;
-  } else if (power < 1) {
-    up = 1;
-    power = 0;
-  }
-  Serial.print(modo);
-  Serial.print(";");
-  Serial.print(up);
-  Serial.print(";");
-  Serial.println(power);
-  analogWrite (leds, power);
+  if (modoOperacao == 1) { 
+    potenciaLed = 255;
+    potenciaCooler = 255;
+  } else if (modoOperacao == 2) { 
+    potenciaLed = 0   ;
+    potenciaCooler = 255;
+  } else if (modoOperacao == 3) { 
+    potenciaLed = 255   ;
+    potenciaCooler = 00;
+  } else if (modoOperacao == 4) { 
+    potenciaLed = 0   ;
+    potenciaCooler = 00;
+  } 
+  
+ 
+  Serial.println(modoOperacao);
+  analogWrite (pinleds, potenciaLed);
+  analogWrite (pinCooler, potenciaCooler);
   delay(100);
 
 }
